590f523fda7580de9c0fd67baa5d77efe1d68fde
Matthew <matthew.lai@laipac.com>
2017-04-16 21:06:27 -0400

    Add utility proc
    Add utility proc

--- web_driver/common/stock_cli.tcl
+++ web_driver/common/stock_cli.tcl
@@ -1,6 +1,23 @@
 source stock.tcl
 source qdo.tcl
 
+proc get_all_data {criterion} {
+
+    set rc ""
+	foreach symbol [stock::get_all_symbols] {
+	    set datalist [stock::get_info_imp $symbol [list $criterion]]
+		set data [lindex $datalist 0]
+		set value [lindex $data 1]
+		if {$value == "nul"} {
+		    continue
+		}
+		if {[lsearch $rc $value] == -1} {
+		    lappend rc $value
+		}
+	}
+    return $rc
+}
+
 proc pprint {data} {
     foreach token $data {
 	puts $token
@@ -23,6 +40,7 @@ proc checkit {dirname} {
 
 proc runit {dirname} {
 	checkit $dirname
+	qdo::init
     set symbols [stock::query_file $dirname/query.txt]
 	foreach symbol $symbols {
 		foreach token [stock::get_info $symbol $dirname/criteria.txt] {
@@ -34,4 +52,3 @@ proc runit {dirname} {
 }
 
 stock::init {stock_T company_T dividend_T}
-qdo::init
