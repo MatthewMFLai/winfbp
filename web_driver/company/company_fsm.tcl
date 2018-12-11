e9785687028d07b380dfe120bf54507c8aa0cbfe
Matthew <matthew.lai@laipac.com>
2018-12-10 22:27:31 -0500

    Use new regexp to extract company's name
    Use new regexp to extract company's name

--- web_driver/company/company_fsm.tcl
+++ web_driver/company/company_fsm.tcl
@@ -32,7 +32,7 @@ proc init {} {
     variable m_data
 
 	set m_rx_list {{description Description<.*?><.*?><.*?>(.*?)<.*?> nul} \
-	               {name Mb\\(10px\\).*?>(.*?)</h3> nul} \
+	               {name \"longName\":\"(.*?)\" nul} \
                    {sector Sector<.*?><.*?>:\\s<.*?><.*?>(.*?)<.*?> nul} \
                    {industry Industry<.*?><.*?>:\\s<.*?><.*?>(.*?)<.*?> nul} \
                    {employees Full\\sTime\\sEmployees<.*?><.*?>:\\s<.*?><.*?><.*?>(.*?)<.*?> nul} \
